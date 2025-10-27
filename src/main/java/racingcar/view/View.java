5b6c52aa408f755f901f742b81d953295791ccc0
suj1124 <suj7874@naver.com>
2025-10-27 15:20:08 +0900

    view 사용자 입력 및 출력 기능 구현

---
+++ src/main/java/racingcar/view/View.java
@@ -0,0 +1,40 @@
+package racingcar.view;
+
+import camp.nextstep.edu.missionutils.Console;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class View {
+
+    public static List<String> readCarNames() {
+        System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
+        String input = Console.readLine();
+
+        // 단순 입력만
+        return Arrays.stream(input.split(",")).map(String::trim).collect(Collectors.toList());
+    }
+
+    public static int readTryCount() {
+        System.out.println("시도할 횟수는 몇 회인가요?");
+        String input = Console.readLine();
+        return Integer.parseInt(input); // 아직 검증은 X
+    }
+
+    public static void printResultTitle() {
+        System.out.println("\n실행 결과");
+    }
+
+
+    public static void printRoundResult(List<String> results) {
+        for (String result : results) {
+            System.out.println(result);
+        }
+        System.out.println();
+    }
+
+    public static void printWinners(List<String> winners) {
+        System.out.println("최종 우승자 : " + String.join(", ", winners));
+    }
+
+}
