87b7cb1d161abb4532e09a999f5c340f6696b9b1
suj1124 <suj7874@naver.com>
2025-10-27 16:55:54 +0900

    RacingController 전체 실행 흐름 구현

--- src/main/java/racingcar/controller/RacingController.java
+++ src/main/java/racingcar/controller/RacingController.java
@@ -1,7 +1,28 @@
 package racingcar.controller;
 
+import racingcar.domain.RacingGame;
+import racingcar.view.View;
+import racingcar.domain.Car;
+import java.util.List;
+
 public class RacingController {
     public void run() {
+        List<String> carNames = View.readCarNames();
+        int tryCount = View.readTryCount();
 
+        RacingGame racingGame = new RacingGame(carNames);
+        View.printResultTitle();
+
+        for (int i = 0; i < tryCount; i++) {
+            racingGame.playRound();
+            View.printRoundResult(
+                    racingGame.getCars().stream()
+                            .map(Car::toString)
+                            .toList()
+            );
+        }
+
+        List<String> winners = racingGame.findWinners();
+        View.printWinners(winners);
     }
 }
